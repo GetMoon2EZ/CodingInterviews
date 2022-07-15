febd7932e27fa77bc19225ca7f9718090aa44426
Luc <luc.techer@sfr.fr>
2022-07-15 13:23:28 +0800

    Add problem template

---
+++ problem_template/solution.c
@@ -0,0 +1,29 @@
+/**
+ * Author:    Luc Techer
+ * Created:   
+ * 
+ * Description: 
+ * Link: 
+ * 
+ * Difficulty: 
+ * Key words: 
+ * 
+ * Compile with: gcc -Wall -Wextra -Wabi solution.c -o solution -g -lm
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+solver()
+{
+
+}
+
+int main()
+{
+    solver();    
+    return 0;
+}
